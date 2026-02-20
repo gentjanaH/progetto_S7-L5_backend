84dd7fef2127ff4dd22e0b97131c23bc9365e410
Gentjana <hanigentjana@gmail.com>
2026-02-20 10:01:31 +0100

    start_progetto

---
+++ u7-l5/src/main/java/gentjanahani/u7_l5/U7L5Application.java
@@ -0,0 +1,13 @@
+package gentjanahani.u7_l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U7L5Application.class, args);
+	}
+
+}
